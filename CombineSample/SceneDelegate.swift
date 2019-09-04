229c532c229294ca20c4eb9082c1b8077c72bfe0
Maria Holubieva <>
2019-09-04 17:04:06 +0300

    simplified switch between swiftUI and UIKit

--- CombineSample/SceneDelegate.swift
+++ CombineSample/SceneDelegate.swift
@@ -14,19 +14,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
 
+    private let swiftUIUsage: Bool = true
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
-        #warning("Uncomment to use UIKit for UI")
-//        guard let _ = (scene as? UIWindowScene) else { return }
-
-        if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: LoginSwiftUIView())
-            self.window = window
-            window.makeKeyAndVisible()
+        if swiftUIUsage {
+            if let windowScene = scene as? UIWindowScene {
+                let window = UIWindow(windowScene: windowScene)
+                window.rootViewController = UIHostingController(rootView: LoginSwiftUIView())
+                self.window = window
+                window.makeKeyAndVisible()
+            }
+        } else {
+            guard let _ = (scene as? UIWindowScene) else { return }
         }
     }
 
